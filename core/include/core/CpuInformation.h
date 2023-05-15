79f1e8064cccbb30e2399e8c61cdcc2ea754fa1b
Anton <>
2023-05-15 10:01:47 +0200

    Reviewed comments

--- core/include/core/CpuInformation.h
+++ core/include/core/CpuInformation.h
@@ -12,9 +12,10 @@ public:
     //returns sum of cores
     uint32_t coreCount() const;
 
+    //returns CPU model, f.e. Intel Core i9-10900F
     std::string model(uint32_t packageId = 0) const;
 
-    //returns frequencey of core with given id in MHz
+    //calculates CPU frequencey (MHz)
     uint32_t frequency() const;
 
 protected:
